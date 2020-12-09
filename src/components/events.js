0d81175035fdd4522bdf81ed7351c0c5738f110b
deannapi <deannapi.mart@gmail.com>
2020-12-09 11:51:05 -0600

    all components created

---
+++ src/components/events.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Events extends React.Component{
+    render() {
+        return (
+            <>
+            <div>
+                
+            </div>
+            </>
+        )
+    }
+}
