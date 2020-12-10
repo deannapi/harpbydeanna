f05d2caf00ff1dbbafe4b94ed759f60028c58b33
deannapi <deannapi.mart@gmail.com>
2020-12-10 08:25:34 -0600

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import deanna_favicon from "../images/favicon-32x32.png";
 
-export default class Footer extends React.Component{
-    render() {
-        return (
-            <>
-            <div>
-                
-            </div>
-            </>
-        )
-    }
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <footer>
+          <h4>Harp By DeAnna</h4>
+          <div className="row">
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/deanna-martinez-63539a137/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.deannapi.com">
+                  <img src={deanna_favicon} alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </>
+    );
+  }
 }
