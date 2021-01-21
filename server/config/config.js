14d688f14d31ba2f13162248945d2985be813a6a
deannapi <deannapi.mart@gmail.com>
2021-01-21 14:01:39 -0600

    cleaned up server folder

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USER: process.env.USER,
-    PASS: process.env.PASS
-}
--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
 var express = require("express");
 var path = require("path");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
 const cors = require("cors");
+var router = express.Router();
+var nodemailer = require("nodemailer");
 
-var transport = require("./routes/index");
-
+const PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(cors());
 
-// app.get('/send', (req, res) => {
-//   res.send('SendMail server is ready.')
-// })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+// Server up static assets
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+if (process.eventNames.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.hmtl"));
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ` + PORT);
-});
+  console.log(`Server running on port ${PORT}.`);
+})
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,40 +36,75 @@ app.use(function (req, res, next) {
   next();
 });
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", index);
-// app.post("/api/sendMail", (req, res) => {
-//   console.log(req.body);
-//   transport(req.body.email, req.body.name, "hello");
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
 // });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+var transport = {
+  service: "gmail",
+  host: "smtp.gmail.com",
+  // port: 465,
+  // secure: true,
+  auth: {
+    // type: "OAuth2",
+    // xoauth2: xoauth2.createXOAuth2Generator({
+    user: process.env.USER,
+    pass: process.env.PASS,
+    // pass: process.env.PASS,
+    // clientId: process.env.OAUTH_CLIENT_ID,
+    // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    // refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    // accessToken: process.env.OAUTH_ACCESS_TOKEN,
+    // expires: 3599,
+  },
+  // ),
+  // },
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages.");
+  }
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+router.post("/send", (req, res, next) => {
+  const mailOptions = {
+    from: email,
+    to: process.env.USER, // Change to email address that you want to receive messages on
+    subject: "Harp Gig Request",
+    html: `<h1>${name} has sent a Harp Gig Request</h1>
+          <p>${email} ${number}</p>
+          <p>${title} ${description}</p>
+          <p>${date} From ${startTime} To ${endTime}</p>
+          <p>Location: ${city}, ${state}</p>
+          <p>Other details: ${details}</p>`,
+  };
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Email sent successfully.");
+    }
+  });
 });
 
 module.exports = app;
