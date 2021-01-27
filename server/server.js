f1fdde7263c6a688ab4982e2ad59facd0ee208f9
deannapi <deannapi.mart@gmail.com>
2021-01-27 10:05:33 -0600

    fixed plain error, now fixing webpack config

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 const cors = require("cors");
 var nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 var app = express();
@@ -23,64 +24,76 @@ app.use(function (req, res, next) {
   next();
 });
 
-// if (process.eventNames.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-
-var transporter = nodemailer.createTransport({
+var transport = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
+  secure: true,
+  service: "gmail",
   auth: {
+    type: "OAuth2",
     user: process.env.USER,
     // pass: process.env.PASS,
     clientId: process.env.OAUTH_CLIENT_ID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.OAUTH_ACCESS_TOKEN,
+    expires: 3599,
   },
 });
 
-transporter.verify((error, success) => {
+function sendMessage() {
+  app.post("/contact", (req, res, next) => {
+    const {
+      name,
+      email,
+      number,
+      title,
+      description,
+      date,
+      startTime,
+      endTime,
+      city,
+      state,
+      details,
+    } = req.body;
+    console.log("it is working", name, email, req.body.number);
+
+    try {
+      // mail options
+      const mailOptions = {
+        from: email,
+        to: process.env.USER, // Change to email address that you want to receive messages on
+        subject: "Harp Gig Request",
+        html: `<h1>req.body.name has sent a Harp Gig Request</h1>
+            <p>Email: req.body.email Number: req.body.number</p>
+            <p>${title} ${description}</p>
+            <p>${date} From ${startTime} To ${endTime}</p>
+            <p>Location: ${city}, ${state}</p>
+            <p>Other details: ${details}</p>`,
+      };
+
+      // send email here
+      transport.sendMail(mailOptions, (err, data) => {
+        if (err) {
+          console.log("Error: ", err);
+        } else {
+          console.log("Email sent successfully.");
+        }
+      });
+    } catch (err) {
+      console.log("There was an error sending the message: ", err);
+    }
+  });
+}
+
+sendMessage();
+
+transport.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
     console.log("Server is ready to take messages.");
   }
-});
-
-app.post("/contact", (req, res, next) => {
-  const {
-    name,
-    email,
-    number,
-    title,
-    description,
-    date,
-    startTime,
-    endTime,
-    city,
-    state,
-    details,
-  } = req.body;
-  console.log("it is working", name, email);
-  const mailOptions = {
-    from: email,
-    to: process.env.USER, // Change to email address that you want to receive messages on
-    subject: "Harp Gig Request",
-    html: `<h1>${name} has sent a Harp Gig Request</h1>
-          <p>${email} ${number}</p>
-          <p>${title} ${description}</p>
-          <p>${date} From ${startTime} To ${endTime}</p>
-          <p>Location: ${city}, ${state}</p>
-          <p>Other details: ${details}</p>`,
-  };
-
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log("Error: ", err);
-    } else {
-      console.log("Email sent successfully.");
-    }
-  });
 });
 
 app.get("*", (req, res) => {
@@ -89,27 +102,7 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
-
-// var transport = {
-// service: "gmail",
-// host: "smtp.gmail.com",
-// port: 465,
-// secure: true,
-// auth: {
-// type: "OAuth2",
-// xoauth2: xoauth2.createXOAuth2Generator({
-// user: process.env.USER,
-// pass: process.env.PASS,
-// clientId: process.env.OAUTH_CLIENT_ID,
-// clientSecret: process.env.OAUTH_CLIENT_SECRET,
-// refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-// accessToken: process.env.OAUTH_ACCESS_TOKEN,
-// expires: 3599,
-// },
-// ),
-// },
-// };
 
 module.exports = app;
